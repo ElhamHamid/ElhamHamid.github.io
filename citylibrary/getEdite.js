867de5267c544ad7c3bd64dd55ee0c19e6a01250
ElhamHamid <69770702+ElhamHamid@users.noreply.github.com>
2021-02-03 22:46:16 -0600

    Update getEdite.js

--- citylibrary/getEdite.js
+++ citylibrary/getEdite.js
@@ -1,29 +1,9 @@
-// let editBook = document.getElementByTagName("editBut");
-// editBook.addEventListener("click", getEditBook);
+"use strict";
+/* eslint-disable*/
 
 
 
 async function getEditBook() {
     alert("hello")
-    // const params = new URLSearchParams(window.location.search);
-    // const bookId = params.get("bookId");
-    // console.log(bookId);
-
-    // const resp = await fetch(`https://elibraryrestapi.herokuapp.com/elibrary/api/book/update/{bookId}`, {
-    //     method: "GET",
-    //     headers: { "content-type": "application/json" },
-
-    // })
-    // const respBody = await resp.json();
-    // //console.log(respBody);
-    // // Obj = respBody;
-    // //let bookId= document.getElementById("bookId").value;
-    // document.getElementById("title").value = respBody.title;
-    // document.getElementById("isbn").value = respBody.isbn;
-    // document.getElementById("publisher").value = respBody.publisher;
-    // document.getElementById("Overdue").value = respBody.overdueFee;
-    // document.getElementById("date").value = respBody.datePublished;
-
-    // console.log(Title.value)
 
 };
